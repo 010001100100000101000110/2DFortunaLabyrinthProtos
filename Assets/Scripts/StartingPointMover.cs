d279d57547a256db5b25b24a89b1591bc83ab76b
Vilma Jalonen <corvuslintu@gmail.com>
2022-09-26 16:18:13 +0300

    starting points

--- Assets/Scripts/StartingPointMover.cs
+++ Assets/Scripts/StartingPointMover.cs
@@ -11,50 +11,35 @@ public class StartingPointMover : MonoBehaviour
 
     [SerializeField] Transform areaPoint1;
     [SerializeField] Transform areaPoint2;
-    [SerializeField] int lerpSchpeed;
 
     void Start()
     {
-        //StartCoroutine(LinearMovementBack());
+        StartCoroutine(LinearMovementBack());
     }
 
     void Update()
     {
         
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
-        {
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-            if (targetObject)
-            {
-                selectedPlayer = targetObject.transform.gameObject;
-                offset = selectedPlayer.transform.position - mousePosition;
-            }
-        }
-        if (selectedPlayer)
-        {
-            //selectedPlayer.transform.position = mousePosition + offset;
-            player.transform.position = Vector3.Lerp(areaPoint1.position, areaPoint2.position, limitMouse());
-        }
-        if (Input.GetMouseButtonUp(0) && selectedPlayer)
-        {
-            selectedPlayer = null;
-        }
-    }
-
-    float limitMouse()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        
-
-        float posX = mousePosition.x;
-        //areaPoint1.position.x / areaPoint2.position.x
-        //posX = mousePosition.x;
-        //if (mousePosition.x < areaPoint1.position.x) posX = areaPoint1.position.x;
-        //if (mousePosition.x > areaPoint2.position.x) posX = areaPoint2.position.x;
-        return posX;
+        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+        //    if (targetObject)
+        //    {
+        //        selectedPlayer = targetObject.transform.gameObject;
+        //        offset = selectedPlayer.transform.position - mousePosition;
+        //    }
+        //}
+        //if (selectedPlayer)
+        //{
+        //    //selectedPlayer.transform.position = mousePosition + offset;
+        //    player.transform.position = Vector3.Lerp(areaPoint1.position, areaPoint2.position, limitMouse());
+        //}
+        //if (Input.GetMouseButtonUp(0) && selectedPlayer)
+        //{
+        //    selectedPlayer = null;
+        //}
     }
 
     //IEnumerator LinearMovementTo()
@@ -66,24 +51,23 @@ public class StartingPointMover : MonoBehaviour
     //    {
     //        ElapsedTime += Time.deltaTime;
     //        player.transform.position = Vector3.Lerp(areaPoint1.position, areaPoint2.position, ElapsedTime/TotalTime );
-           
+
     //    }
     //   //StartCoroutine(LinearMovementBack());
     //    yield return null;
     //}
 
-    //IEnumerator LinearMovementBack()
-    //{
-    //    float ElapsedTime = 3;
-    //    float TotalTime = 0;
+    IEnumerator LinearMovementBack()
+    {
+        float ElapsedTime = 3;
+        float TotalTime = 0;
 
-    //    while (ElapsedTime > TotalTime)
-    //    {
-    //        ElapsedTime -= Time.deltaTime;
-    //        player.transform.position = Vector3.Lerp(areaPoint1.position, areaPoint2.position, TotalTime / ElapsedTime);
-            
-    //    }
-    //    //StartCoroutine(LinearMovementTo());
-    //    yield return null;
-    //}
+        while (ElapsedTime > TotalTime)
+        {
+            ElapsedTime -= Time.deltaTime;
+            player.transform.position = Vector3.Lerp(areaPoint1.position, areaPoint2.position, TotalTime / ElapsedTime);
+        }
+        //StartCoroutine(LinearMovementTo());
+        yield return null;
+    }
 }
